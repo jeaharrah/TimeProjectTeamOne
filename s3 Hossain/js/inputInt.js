d9b22495556b05d7676949a8bd01ea891f33af40
Shakhawat Hossain <sxh599@psu.edu>
2018-12-06 20:26:59 -0500

    Update inputInt.js

--- s3 Hossain/js/inputInt.js
+++ s3 Hossain/js/inputInt.js
@@ -9,7 +9,7 @@ function validate(evt) {
       var key = theEvent.keyCode || theEvent.which;
       key = String.fromCharCode(key);
   }
-  var regex = /[0-9]|\./;
+  var regex = /[0-9]/;
   if( !regex.test(key) ) {
     theEvent.returnValue = false;
     if(theEvent.preventDefault) theEvent.preventDefault();
